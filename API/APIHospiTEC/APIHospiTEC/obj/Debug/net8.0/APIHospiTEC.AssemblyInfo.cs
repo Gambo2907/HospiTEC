4c007e4dc48ce6cb67e88e9c8cba930159d83729
Gambo2907 <sgamo2220@gmail.com>
2024-05-26 01:46:17 -0600

    1

--- API/APIHospiTEC/APIHospiTEC/obj/Debug/net8.0/APIHospiTEC.AssemblyInfo.cs
+++ API/APIHospiTEC/APIHospiTEC/obj/Debug/net8.0/APIHospiTEC.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("APIHospiTEC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4290db6783f2bcaf54067c91409c3f55856a728f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6aa7f80e0db422bf331a2c7d7ed658ad6934969f")]
 [assembly: System.Reflection.AssemblyProductAttribute("APIHospiTEC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("APIHospiTEC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
