6aa7f80e0db422bf331a2c7d7ed658ad6934969f
Gambo2907 <sgamo2220@gmail.com>
2024-05-26 01:38:35 -0600

    Modelo login, modelo paciente

---
+++ API/APIHospiTEC/Paciente.cs
@@ -0,0 +1,16 @@
+﻿
+namespace APIHospiTEC.Models
+{
+    public class Paciente
+    {
+        public required string Nombre { get; set; }
+        public required string Ap1 { get; set; }
+        public required string Ap2 { get; set; }
+        public required int Cedula { get; set; }
+        public DateOnly Direccion { get; set; }
+        public int? Telefono { get; set; }
+        public string? Correo { get; set; }
+        public string? Password { get; set; }
+    }
+}
+
