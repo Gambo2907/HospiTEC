066e4cf634ca0163e60987b1bdd2b7275bc53cd8
Gambo2907 <sgamo2220@gmail.com>
2024-05-27 22:44:38 -0600

    Scripts

---
+++ Scripts/ScriptPopulacionDB.sql
@@ -0,0 +1,69 @@
+INSERT INTO personal.TIPO_PERSONAL
+VALUES(1,'Doctor');
+
+INSERT INTO personal.TIPO_PERSONAL
+VALUES(2,'Enfermero');
+
+INSERT INTO personal.TIPO_PERSONAL
+VALUES(3,'Administrativo');
+
+INSERT INTO salones.TIPO_MEDICINA
+VALUES(1,'Hombres');
+
+INSERT INTO salones.TIPO_MEDICINA
+VALUES(2,'Mujeres');
+
+INSERT INTO salones.TIPO_MEDICINA
+VALUES(3,'Niños');
+
+INSERT INTO salones.ESTADO_CAMA
+VALUES(1,'Disponible');
+
+INSERT INTO salones.ESTADO_CAMA
+VALUES(2,'Reservado');
+
+INSERT INTO salones.EQUIPO_MEDICO
+VALUES(1,'Luces Quirurgicas','Kalstein',50);
+
+INSERT INTO salones.EQUIPO_MEDICO
+VALUES(2,'Ultrasonido','Eleinmsa',45);
+
+INSERT INTO salones.EQUIPO_MEDICO
+VALUES(3,'Esterilizadores','Eleinmsa',25);
+
+INSERT INTO salones.EQUIPO_MEDICO
+VALUES(4,'Desfibriladores','Eleinmsa',25);
+
+INSERT INTO salones.EQUIPO_MEDICO
+VALUES(5,'Monitores','Eleinmsa',50);
+
+INSERT INTO salones.EQUIPO_MEDICO
+VALUES(6,'Respiradores Artificiales','Eleinmsa',75);
+
+INSERT INTO salones.EQUIPO_MEDICO
+VALUES(7,'Electrocardiografos','Eleinmsa',65);
+
+INSERT INTO reservacion.PROCEDIMIENTO_MEDICO
+VALUES(1,'Apendicectomia',7);
+
+INSERT INTO reservacion.PROCEDIMIENTO_MEDICO
+VALUES(2,'Biopsia de Mama',5);
+
+INSERT INTO reservacion.PROCEDIMIENTO_MEDICO
+VALUES(3,'Cirugia de Cataratas',2);
+
+INSERT INTO reservacion.PROCEDIMIENTO_MEDICO
+VALUES(4,'Cesarea',3);
+
+INSERT INTO reservacion.PROCEDIMIENTO_MEDICO
+VALUES(5,'Histerectomia',6);
+
+INSERT INTO reservacion.PROCEDIMIENTO_MEDICO
+VALUES(6,'Cirugia para la Lumbalgia',12);
+
+INSERT INTO reservacion.PROCEDIMIENTO_MEDICO
+VALUES(7,'Mastectomia',5);
+
+INSERT INTO reservacion.PROCEDIMIENTO_MEDICO
+VALUES(8,'Amigdalectomia',1);
+
